241e3b099fe03a7fffdf54423fd00cc727b000d9
Bogdan Czajka <bchaika95@gmail.com>
2026-01-12 18:54:49 +0100

    Changed expectation for new group to contain teams with 0 stats.

--- src/test/java/org/scoreboard/model/GroupDataTest.java
+++ src/test/java/org/scoreboard/model/GroupDataTest.java
@@ -2,6 +2,9 @@ package org.scoreboard.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GroupDataTest {
@@ -11,5 +14,19 @@ class GroupDataTest {
         GroupData group = new GroupData();
 
         assertTrue(group.getStandings().isEmpty());
+    }
+
+    @Test
+    void shouldCreateStandings_WhenFirstMatchAdded() {
+        GroupData group = new GroupData();
+        MatchUpdate match = new MatchUpdate("m1", "PL", "EN", 2, 1, true, false);
+
+        GroupData updatedGroup = group.updateMatch(match);
+        Map<String, TeamStanding> table = updatedGroup.getStandings();
+
+        assertEquals(2, table.size());
+        assertEquals(3, table.get("PL").points());
+        assertEquals(0, table.get("EN").points());
+        assertEquals(1, table.get("PL").played());
     }
 }
