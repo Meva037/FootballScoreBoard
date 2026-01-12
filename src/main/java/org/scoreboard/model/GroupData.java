46bcfc41123b61cbf44657454e8cb1c81b15021f
Bogdan Czajka <bchaika95@gmail.com>
2026-01-12 18:58:49 +0100

    Implemented GroupData creation.

--- src/main/java/org/scoreboard/model/GroupData.java
+++ src/main/java/org/scoreboard/model/GroupData.java
@@ -1,13 +1,21 @@
 package org.scoreboard.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupData {
+    private final Map<String, MatchUpdate> matches;
     private final Map<String, TeamStanding> standings;
+    private final Set<String> registeredTeamIds;
 
-    public GroupData() {
-        standings = new HashMap<>();
+    public GroupData(Collection<String> teamIds) {
+        this.matches = Map.of();
+        this.registeredTeamIds = new HashSet<>(teamIds);
+        this.standings = teamIds.stream()
+                .collect(Collectors.toMap(
+                        id -> id,
+                        TeamStanding::empty
+                ));
     }
 
     public Map<String, TeamStanding> getStandings() {
