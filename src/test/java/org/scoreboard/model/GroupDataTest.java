1640f3eeafd791d0a9859a212f5ea843af84ec7e
Bogdan Czajka <bchaika95@gmail.com>
2026-01-12 19:24:52 +0100

    Added aggregation test for GroupData.

--- src/test/java/org/scoreboard/model/GroupDataTest.java
+++ src/test/java/org/scoreboard/model/GroupDataTest.java
@@ -20,14 +20,9 @@ class GroupDataTest {
         assertEquals(4, table.size());
 
         teamIds.forEach(teamId -> assertTrue(table.containsKey(teamId)));
-        TeamStanding pl = table.get("PL");
-        assertEquals(0, pl.points());
-        assertEquals(0, pl.played());
-        assertEquals(0, pl.goalsScored());
-        assertEquals(0, pl.goalsConceded());
-        assertEquals(0, pl.won());
-        assertEquals(0, pl.drawn());
-        assertEquals(0, pl.lost());
+        TeamStanding plStats = table.get("PL");
+        assertTeamStandingEquals(0, 0, 0, 0, 0, 0, 0, plStats);
+
     }
 
     @Test
@@ -39,11 +34,48 @@ class GroupDataTest {
         Map<String, TeamStanding> table = group.getStandings();
 
         assertEquals(4, table.size());
-        assertEquals(3, table.get("PL").points());
-        assertEquals(2, table.get("PL").goalsDifference());
-        assertEquals(0, table.get("EN").points());
-        assertEquals(-2, table.get("EN").goalsDifference());
-        assertEquals(0, table.get("IT").points());
-        assertEquals(0, table.get("IT").goalsDifference());
+        TeamStanding plStats = table.get("PL");
+        TeamStanding enStats = table.get("EN");
+        TeamStanding itStats = table.get("IT");
+        assertTeamStandingEquals(1, 3, 2, 0, 1, 0, 0, plStats);
+        assertTeamStandingEquals(1, 0, 0, 2, 0, 0, 1, enStats);
+        assertTeamStandingEquals(0, 0, 0, 0, 0, 0, 0, itStats);
+    }
+
+    @Test
+    void shouldAggregatePoints_FromDifferentMatches() {
+        GroupData group = new GroupData(List.of("PL", "EN", "IT", "FR"));
+        group = group.updateMatch(new MatchUpdate("m1", "PL", "EN", 1, 0, true, true));
+        group = group.updateMatch(new MatchUpdate("m2", "PL", "IT", 1, 1, true, true));
+
+        Map<String, TeamStanding> table = group.getStandings();
+
+        TeamStanding plStats = table.get("PL");
+        TeamStanding enStats = table.get("EN");
+        TeamStanding itStats = table.get("IT");
+        TeamStanding frStats = table.get("FR");
+        assertTeamStandingEquals(2, 4, 2, 1, 1, 1, 0, plStats);
+        assertTeamStandingEquals(1, 0, 0, 1, 0, 0, 1, enStats);
+        assertTeamStandingEquals(1, 1, 1, 1, 0, 1, 0, itStats);
+        assertTeamStandingEquals(0, 0, 0, 0, 0, 0, 0, frStats);
+    }
+
+    private void assertTeamStandingEquals(
+            int expectedPlayed,
+            int expectedPoints,
+            int expectedGoalsScored,
+            int expectedGoalsConceded,
+            int expectedWon,
+            int expectedDrawn,
+            int expectedLost,
+            TeamStanding current
+    ) {
+        assertEquals(expectedPlayed, current.played());
+        assertEquals(expectedPoints, current.points());
+        assertEquals(expectedGoalsScored, current.goalsScored());
+        assertEquals(expectedGoalsConceded, current.goalsConceded());
+        assertEquals(expectedWon, current.won());
+        assertEquals(expectedDrawn, current.drawn());
+        assertEquals(expectedLost, current.lost());
     }
 }
