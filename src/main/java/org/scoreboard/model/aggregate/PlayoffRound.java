02e83f19892f21d943a92174937a897a10338f79
Bogdan Czajka <bchaika95@gmail.com>
2026-01-12 21:49:15 +0100

    Implemented PlayoffRound initialization.

--- src/main/java/org/scoreboard/model/aggregate/PlayoffRound.java
+++ src/main/java/org/scoreboard/model/aggregate/PlayoffRound.java
@@ -1,4 +1,25 @@
 package org.scoreboard.model.aggregate;
 
+import org.scoreboard.model.MatchUpdate;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PlayoffRound {
+
+    private final Map<String, MatchUpdate> matches;
+
+    public PlayoffRound(List<MatchUpdate> initialMatches) {
+        this.matches = new LinkedHashMap<>();
+        for (MatchUpdate m : initialMatches) {
+            this.matches.put(m.matchId(), m);
+        }
+    }
+
+    public List<MatchUpdate> getMatchesList() {
+        return new ArrayList<>(matches.values());
+    }
+
 }
